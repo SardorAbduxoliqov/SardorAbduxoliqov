e3bfda3b2088e6fe3972e6bcc9d72c451d2138d7
SardorAbduxoliqov <sardornewgithub@mail.ru>
2021-07-14 00:02:11 -0600

    changed render to json form

--- src/controllers/taskController.ts
+++ src/controllers/taskController.ts
@@ -5,9 +5,7 @@ const  getListOfImage = async (req: Request, res: Response,next: NextFunction) =
   const url_api = 'https://random.dog/woof.json';
   try {
     const imageData = await axios.get(url_api);
-    res.status(200).render('tasks/list', {
-      data: imageData.data,
-    });
+    res.status(200).json({ data: imageData.data });
   } catch (error) {
     console.log(error.message);
   }
