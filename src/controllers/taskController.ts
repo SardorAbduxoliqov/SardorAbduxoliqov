15f0022cf15724a4189863361de926a144697349
SardorAbduxoliqov <sardornewgithub@mail.ru>
2021-07-14 22:53:28 -0600

    search task added

--- src/controllers/taskController.ts
+++ src/controllers/taskController.ts
@@ -1,3 +1,6 @@
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable func-names */
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import { Request, Response } from 'express';
 import axios from 'axios';
@@ -17,13 +20,36 @@ const getListOfImage = async (req: Request, res: Response) => {
 
 // eslint-disable-next-line consistent-return
 const getAll = async (req: Request, res: Response) => {
-  try {
-    const images = await Task.find().sort({ fileSizeBytes: 1 });
-    console.log(images[1]);
-    return res.render('tasks/all', { data: images, path: '/all' });
-  } catch (error) {
-    console.log(error.message);
-  }
+    if(req.query.search) {
+        const regex = new RegExp(escapeRegex(req.query.search), 'gi');
+        // Get all campgrounds from DB
+       await Task.find({fileSizeBytes: regex}, function(err, url){
+           if(err){
+               console.log(err);
+           } else {
+              if(url.length < 1) {
+                const message = "this size image not found"
+              }
+                res.render("tasks/all",{data: url});
+           }
+        });
+    } else {
+        // Get all campgrounds from DB
+        await Task.find({}, function(err, allImages){
+           if(err){
+               console.log(err);
+           } else {
+              res.render("tasks/all",{data:allImages});
+           }
+        });
+    }
+  // try {
+  //   const images = await Task.find().sort({ fileSizeBytes: 1 });
+  //   console.log(images[1]);
+  //   return res.render('tasks/all', { data: images, path: '/all' });
+  // } catch (error) {
+  //   console.log(error.message);
+  // }
 };
 
 const postListOfImage = async (
@@ -40,6 +66,11 @@ const postListOfImage = async (
   } catch (error) {
     console.log(error.message);
   }
+};
+
+
+function escapeRegex(text: RegExpExecArray ) {
+  return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
 };
 
 export default {
